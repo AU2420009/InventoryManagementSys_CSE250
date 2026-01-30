52aba249b52af99312d1c64d2c8882e9ff749b2f
Ankit-Kumarh7 <ankit97963@gmail.com>
2026-01-30 15:06:06 +0530

    Add frontend html

---
+++ tree/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the inventory management System");
+        sc.close();
+    }
+}
