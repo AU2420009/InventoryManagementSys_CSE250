2086c2ca6efaa1b86d5796f299b0f7f0c3b10a86
Anant Narayan <anant.n@ahduni.edu.in>
2026-04-17 00:33:00 +0530

    fixed drop db

--- db/oms-drop.sql
+++ db/oms-drop.sql
@@ -1,1 +1,13 @@
+-- drop child tables first to prevent foreign key errors
+
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS products;
+
+-- drop RBAC tables
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS roles;
+
 DROP DATABASE IF EXISTS inventory_db;
